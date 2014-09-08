0a4d3ee259ff4dc1d1dbb888cdb31aecf0a2d9ec
zym242 <zymfrank@hotmail.com>
2014-09-08 21:17:48 +0930

    test2

--- hello.js
+++ hello.js
@@ -1,3 +1,15 @@
+$(function(){
 $('.loginBtn').click(function(){
-console.log("test");
+FB.getLoginStatus(function(response) {
+  if (response.status === 'connected') {
+    console.log('Logged in.');
+  }
+  else {
+    FB.login();
+  }
+});
+});
+$('#loginBtn').click(function(){
+console.log("test2");
+})
 })
